d92fd431f1bf3498e2563c0f98432aa2f38fa0ae
JoshuaPaino <joshua.paino@gmail.com>
2022-10-02 20:42:07 -0500

    Finish Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
+  get 'static_page/home' 
   get 'static_page/help'
+  get 'static_page/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "application#hello"
+  root "static_page#home"
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,10 +4,19 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
+  
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
 end
