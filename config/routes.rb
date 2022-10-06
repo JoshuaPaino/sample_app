7b5b17bfc7f9d109c4dc6d9358b2198ce94dcd4d
JoshuaPaino <joshua.paino@gmail.com>
2022-10-06 16:37:45 -0500

    Logging Finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "static_page#home"
   get  "/help",    to: "static_page#help"
   get  "/about",   to: "static_page#about"
   get  "/contact", to: "static_page#contact"
-
-
   get "signup", to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
